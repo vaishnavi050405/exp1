c01e8c7ff05879426627719d1a00274a88d3dd4a
vaishnavi050405 <vaishnavishintre95@gmail.com>
2025-01-16 23:12:02 -0800

    Add files via upload

---
+++ exp1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+ 	int a,b,sum,sub,mul,div;
+ 	std::cout<<"enter values of a & b:"<<"\n";
+ 	std::cin>>a>>b;
+ 	sum=a+b;
+ 	std::cout<<"the sum is:"<<sum<<"\n";
+ 	sub=a-b;
+ 	std::cout<<"the sub is:"<<sub<<"\n";
+ 	mul=a*b;
+ 	std::cout<<"the mul is:"<<mul<<"\n";
+ 	div=a/b;
+ 	std::cout<<"the div is:"<<div<<"\n";
+}
